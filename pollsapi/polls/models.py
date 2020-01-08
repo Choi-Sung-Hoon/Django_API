ef60904dab2365811f5ce7046b3ff8d4d4f4ba32
Choi-Sung-Hoon <tjdgns9246@naver.com>
2020-01-08 10:32:54 +0000

    Fix indentation and typo for error

--- pollsapi/polls/models.py
+++ pollsapi/polls/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class poll(models.Model):
+class Poll(models.Model):
 	question = models.CharField(max_length=100)
 	created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 	pub_date = models.DateTimeField(auto_now=True)
@@ -15,7 +15,7 @@ class Choice(models.Model):
 	choice_text = models.CharField(max_length=100)
 
 	def __str__(self):
-	return self.choice_text
+		return self.choice_text
 
 class Vote(models.Model):
 	choice = models.ForeignKey(Choice, related_name='votes', on_delete=models.CASCADE)
